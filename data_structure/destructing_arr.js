e8e8841a24f8811ee9f84521cec7367ba3240a58
avtar <avtarakhrisna1@gmail.com>
2021-07-09 04:00:36 +0700

    feat: destructing array

--- data_structure/destructing_arr.js
+++ data_structure/destructing_arr.js
@@ -1,6 +1,13 @@
 const number = [1, 2, 3, 4, 5];
 
-//const [one, two, three, four] = number;
-const [,, three, four] = number;
+const [one, two, three, four] = number;
+console.log(one, two, three, four); // output: 1, 2, 3, 4, 5
 
-console.log(three,four);
+const [,, tiga, empat] = number;
+console.log(tiga, empat); //ouput: 3, 4
+
+a = 10;
+b = 20;
+console.log(a, b); // output: 10 20
+[a, b] = [b, a];
+console.log(a, b); // output: 20 10
